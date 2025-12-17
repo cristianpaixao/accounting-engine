07df20544c1c713c0c8e2e3d3482a07e6794ab4a
Cristian Paixão <cristian.paixao@outlook.com>
2025-12-17 16:54:12 -0300

    feat(wallet): add money dataclass

---
+++ app/domain/shared/value_objects.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from decimal import Decimal
+
+@dataclass(frozen=True)
+class Money:
+    amount: Decimal
+    asset: str
+
+    def __post_init__(self):
+        if self.amount < 0:
+            raise ValueError("Amount must be positive")
